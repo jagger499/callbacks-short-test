f05dd54a20df30c85c037196cf48ddefd7a560fb
jhon garcia <jagg499@gmail.com>
2021-11-30 15:42:34 -0500

    short syntax error correction

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -2,16 +2,19 @@ let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 //funcion para obtener los datos usando xmlhttprequest parametros url de la api y callback function
 function fetchData(url_api , callback){
+
     //asigno un shortcut a xmlhttprequest
     let xhttp = new XMLHttpRequest;
     xhttp.open('GET',url_api ,true);
+
     //si esta listo
     xhttp.readystatechange = function (event){
         if(xhttp.readyState === 4){
+
             //si la respuesta fue un codigo 200
             if(xhttp.status === 200){
                 callback(null, JSON.parse(xhttp.responseText));
-            }else(){
+            }else{
                 const error = new Error('error '+ url_api)
                 return callback(error, null)
             }
