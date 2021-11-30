df36b002f361717b610d22833a209d55ab00dd24
jhon garcia <jagg499@gmail.com>
2021-11-30 16:54:11 -0500

    uso basico de promesas sin consumo de apis en promise/index.js

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,3 +1,4 @@
+//promesa que se ejecuta validando el if
 const sometimesWillHappen = () =>{
     return new Promise((resolve, reject) => {
         if(true){
@@ -8,24 +9,37 @@ const sometimesWillHappen = () =>{
     })
 } 
 
+//ejecutando la promesa haciendo un console.log de la respuesta o el error
 sometimesWillHappen()
     .then(response => console.log(response))
     .catch(error => console.error(error))
 
 
+//promesa que se ejecuta validando el if si se valida tiene un tiempo de 3s para entregar la respuesta
 const sometimesWillHappen2 = () => {
     return new Promise((resolve,reject)=>{
         if(true){
            setTimeout(() => {
-            resolve('hey')
+            resolve('casi')
            }, 3000); 
         }else{
-            const error = new error('nooooo')
-            reject('error')
+            const error = new error('nooooo');
+            reject('error');
         }
     })
 }
 
+//ejecutando la promesa haciendo un console.log de la respuesta o el error
 sometimesWillHappen2()
     .then(response => console.log(response))
     .catch(err => console.error(err))
+
+
+//ejecutando las dos promesas de forma simultanea y haciendo un console.log de sus respuestas
+Promise.all([sometimesWillHappen(),sometimesWillHappen2()])
+    .then(response => {
+        console.log("results array: "+ response)
+    })
+    .catch(err =>{
+        console.error(err);
+    })
